bc9f8e992e3bfd410b251fd7be156bcce79a60ca
Sofía <ravennasofia03@gmail.com>
2023-04-21 16:13:03 -0300

    intercamio valores de variables con punteros

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
+/*Escriba una función que intercambie el valor de dos variables enteras utilizando
+punteros y los muestre antes y después del intercambio por pantalla.*/
+
 #include <stdio.h>
 
 int main( )
 {
-    printf("Hello, World!\n");
+    int x=1;
+    int y=2;
+    int z;
+
+    int *pa=&x;
+    int *pb=&y;
+
+    printf("El valor de x es: %d\nEL valor de y es: %d\n",x,y);
+
+    z=*pa;
+    x=*pb;
+    y=z;
+
+    printf("El nuevo valor de x es: %d\nEl nuevo valor de y es: %d",x,y);
     return 0;
 }
